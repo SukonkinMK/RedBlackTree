3da0042f11bf266b3d50b69a675d9150081ad261
SukonkinMK <sukonkin.mcpotap@yandex.ru>
2023-02-26 16:19:29 +0300

    Реализовано красно-черное дерево. Реализован метод добавления новых элементов с балансировкой

--- RedBlackTree/Program.cs
+++ RedBlackTree/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RedBlackTree;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        RedBlackTree tree = new RedBlackTree();
+        string input;
+        while (true)
+        {
+            Console.Write("Введите значение узла дерева: ");
+            input = Console.ReadLine();
+            if(int.TryParse(input, out int result))
+            {
+                tree.add(result);
+                Console.WriteLine("done");
+            }
+            else
+            {
+                if (input.Equals("exit"))
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
